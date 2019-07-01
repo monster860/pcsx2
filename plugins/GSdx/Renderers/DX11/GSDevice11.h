618a22b7d64def45d8a228d6070b4b4ad3c21c31
lightningterror <lightningterror@yahoo.com>
2019-07-02 00:30:30 +0200

    Gsdx-d3d11: Experimental Domerge feedback write port.
    WIP.

--- plugins/GSdx/Renderers/DX11/GSDevice11.h
+++ plugins/GSdx/Renderers/DX11/GSDevice11.h
@@ -312,6 +312,13 @@ public:
 		OMBlendSelector() : key(0) {}
 	};
 
+	struct alignas(32) MiscConstantBuffer
+	{
+		GSVector4i EMOD_AC;
+
+		MiscConstantBuffer() {memset(this, 0, sizeof(*this));}
+	};
+
 	#pragma pack(pop)
 
 	class ShaderMacro
@@ -410,6 +417,7 @@ private:
 		CComPtr<ID3D11DepthStencilState> dss;
 		CComPtr<ID3D11DepthStencilState> dss_write;
 		CComPtr<ID3D11BlendState> bs;
+		CComPtr<ID3D11Buffer> cb;
 	} m_convert;
 
 	struct
@@ -466,6 +474,7 @@ private:
 	VSConstantBuffer m_vs_cb_cache;
 	GSConstantBuffer m_gs_cb_cache;
 	PSConstantBuffer m_ps_cb_cache;
+	MiscConstantBuffer m_misc_cb_cache;
 
 	std::unique_ptr<GSTexture> m_font;
 
